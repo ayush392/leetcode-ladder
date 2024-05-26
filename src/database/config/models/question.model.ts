17c48a60cae99b57d3349e87255b29a7789df8a7
ayush392 <ayushgoyal.0004@gmail.com>
2024-05-27 05:00:36 +0530

    feat: add dbconnection and model

---
+++ src/database/config/models/question.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+  contest: {
+    title: String,
+    titleSlug: String,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  titleSlug: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    enum: ["Easy", "Medium", "Hard"],
+  },
+  credit: {
+    type: Number,
+    required: true,
+  },
+  questionId: {
+    type: String,
+    required: true,
+  },
+});
+
+const questionModel =
+  mongoose.models.Question || mongoose.model("Question", questionSchema);
+
+export default questionModel;
