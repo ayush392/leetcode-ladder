93974cd8f72b4c6aa2b99ce7f742931d4c50adab
ayush392 <ayushgoyal.0004@gmail.com>
2024-05-28 03:24:46 +0530

    minor bug fix

--- src/components/DropdownButton.tsx
+++ src/components/DropdownButton.tsx
@@ -16,7 +16,7 @@ function DropdownButton({buttonName, dropdownItems, handleChange, isOpen, setIsO
     <div>
       <button
         onClick={() => {isOpen === buttonName ? setIsOpen("") : setIsOpen(buttonName)}}
-        onBlur={() => setIsOpen("")}
+        // onBlur={() => {isOpen === buttonName ? setIsOpen("") : setIsOpen(buttonName)}}
         className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-1 focus:ring-gray-200 font-medium rounded-md text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
         type="button"
       >
