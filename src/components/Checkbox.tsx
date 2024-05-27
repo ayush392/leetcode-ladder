ea5a2cbafc245f08337caf2de3bd0d2268fc5d67
ayush392 <ayushgoyal.0004@gmail.com>
2024-05-28 01:02:20 +0530

    style: checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Checkbox() {
-  return <input type="checkbox" />;
+  return <input className="cursor-pointer scale-125" type="checkbox" />;
 }
 
 export default Checkbox;
