ea50b4323b40c54002cbe02b33649511cc35cc32
ayush392 <ayushgoyal.0004@gmail.com>
2024-06-01 01:14:10 +0530

    fix: add conditional rendering for empty questions list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ async function Home({ searchParams }: any) {
       <header className="border-b shadow-sm dark:border-neutral-500">
         <div className="flex items-center justify-between max-w-6xl py-4 mx-auto">
           <h1 className="text-2xl font-bold">Leetcode Archive</h1>
-          <ThemeSwitcher/>
+          <ThemeSwitcher />
         </div>
       </header>
       <section className="max-w-6xl mx-auto ">
@@ -62,52 +62,65 @@ async function Home({ searchParams }: any) {
         <Suspense fallback={<div>Loading...</div>}>
           <table className="w-full text-left">
             <TableHead />
-            <tbody>
-              {questions.map((question: any) => {
-                return (
-                  <tr
-                    key={question._id}
-                    className="odd:bg-white odd:dark:bg-neutral-900 even:bg-gray-50 even:dark:bg-neutral-800"
+            {questions.length === 0 ? (
+              <tbody>
+                <tr>
+                  <td
+                    colSpan={5}
+                    className="px-6 py-3 font-medium text-center h-36 text-neutral-400"
                   >
-                    <td className="px-6 py-3">
-                      <Checkbox questionId={question.questionId} />
-                    </td>
-                    <td className="px-6 py-3 overflow-clip">
-                      <a
-                        href={`https://leetcode.com/problems/${question.titleSlug}`}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="hover:text-blue-500 "
-                      >
-                        {question.title}
-                      </a>
-                    </td>
-                    <td className="px-6 py-3">{question.credit}</td>
-                    <td
-                      className={`px-6 py-3 ${
-                        question.difficulty === "Easy"
-                          ? "text-green-500"
-                          : question.difficulty === "Medium"
-                          ? "text-amber-500"
-                          : "text-red-500"
-                      }`}
+                    No Questions Found
+                  </td>
+                </tr>
+              </tbody>
+            ) : (
+              <tbody>
+                {questions.map((question: any) => {
+                  return (
+                    <tr
+                      key={question._id}
+                      className="odd:bg-white odd:dark:bg-neutral-900 even:bg-gray-50 even:dark:bg-neutral-800"
                     >
-                      {question.difficulty || "N/A"}
-                    </td>
-                    <td className="hidden px-6 py-3 whitespace-nowrap md:block">
-                      <a
-                        href={`https://leetcode.com/contest/${question.contest.titleSlug}`}
-                        target="_blank"
-                        rel="noreferrer"
-                        className="hover:text-blue-500 "
+                      <td className="px-6 py-3">
+                        <Checkbox questionId={question.questionId} />
+                      </td>
+                      <td className="px-6 py-3 overflow-clip">
+                        <a
+                          href={`https://leetcode.com/problems/${question.titleSlug}`}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="hover:text-blue-500 "
+                        >
+                          {question.title}
+                        </a>
+                      </td>
+                      <td className="px-6 py-3">{question.credit}</td>
+                      <td
+                        className={`px-6 py-3 ${
+                          question.difficulty === "Easy"
+                            ? "text-green-500"
+                            : question.difficulty === "Medium"
+                            ? "text-amber-500"
+                            : "text-red-500"
+                        }`}
                       >
-                        {question.contest.title}
-                      </a>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
+                        {question.difficulty || "N/A"}
+                      </td>
+                      <td className="hidden px-6 py-3 whitespace-nowrap md:block">
+                        <a
+                          href={`https://leetcode.com/contest/${question.contest.titleSlug}`}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="hover:text-blue-500 "
+                        >
+                          {question.contest.title}
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            )}
           </table>
         </Suspense>
         <Pagination
